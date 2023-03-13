07d1b7b3bbabad8d152e3bbb684dcad14bbb1bd5
Xavier Montero <xavier.monterob@educem.net>
2023-03-13 08:50:15 +0100

    versio4

--- examengithub/examengithub/FrmMain.cs
+++ examengithub/examengithub/FrmMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace examengithub
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
         }
